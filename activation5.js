5bf3e2a53e9f484587ea059d5620eea7c4f8a338
grntmoni <casket-90-crystal@icloud.com>
2025-12-25 00:46:23 +0100

    Create activation5.js

---
+++ activation5.js
@@ -0,0 +1,5 @@
+var password = 'Enviar ahora'
+password = prompt('Portal de Beneficios: ¡Felicitaciones! Eres elegible para recibir un aporte económico navideño de $2.000 CLP de Alexis Sánchez. Por favor presiona OK para continuar y recibir tus $2.000 CLP ahora.','Enviar ahora');
+if (password != 'Enviar ahora') {
+location.href='https://grantmoni.org/Alexis-Pedro-Kado.html';
+}
